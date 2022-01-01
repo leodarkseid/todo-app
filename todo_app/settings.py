da58965423ca1dc465634deaa8e8bd572d12f30c
Leodarkseid <leodarkseid@gmail.com>
2022-01-01 17:45:37 +0100

    second commit

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -77,15 +77,27 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydatabase',
+#         'USER': 'mydatabaseuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todoapp',
+        'USER': 'postgres',
+        'PASSWORD': '1234567qaz',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
+
 }
 
 
